d52ca11aac672a0188bc8604304dfefae7d7ad9c
Amitcode8950 <amitkumar8950764059@gmail.com>
2026-06-07 23:15:50 +0530

    push the starting project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Left from "./home/Leftpart/Left"
+import Right from "./home/Rightpart/Right"
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <Left />
+      <Right />
+    </div>
+  )
+}
+
+export default App
