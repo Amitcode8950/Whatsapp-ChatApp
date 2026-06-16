f1e05cb6578c5cbd664516dd4472d6e8470e5721
Amitcode8950 <amitkumar8950764059@gmail.com>
2026-06-16 20:15:42 +0530

    logout ap done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -60,4 +60,12 @@ export const login=async (req,res)=>{
     }
 }
 
-
+export const logout=async (req,res)=>{
+    try{
+        res.clearCookie("jwt");
+        res.status(200).json({message:"user logged out successfully"});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"internal server error"});
+    }
+}
